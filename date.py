f54d37208e534dca66bace723e773218b509bc55
Dave McConeghy <david.mcconeghy@gmail.com>
2022-03-12 10:32:26 -0500

    Adds more code that has been refactored out

--- date.py
+++ date.py
@@ -99,3 +99,93 @@
     #         .order_by(ChartAppearance.chart_date.desc())
     #         .all())
     # results = zip(songs, charts)
+
+# song_ids = [s.song_id for s in chart.songs]
+        # appearance_objects = [a for a in chart.songs]
+
+        # sort the list of appearances by their ranks
+        # appearance_objects.sort(key=lambda x: x.rank)
+
+#                 
+        # song_objects = [(Song
+        #                 .query
+        #                 .join(ChartAppearance, ChartAppearance.song_id == sid)
+        #                 .filter(ChartAppearance.chart_date == chart_date)
+        #                 .order_by(ChartAppearance.rank)
+        #                 .all()
+        #                 ) for sid in song_ids]
+
+# chart_date = chart.chart_date.isoformat()
+
+# response = Song.query.order_by(Song.id).all()
+
+    # for song in all_songs:
+    #     # Check if artist page has been searched for
+    #     if song.artist_page == "Not Queried":
+            
+    #         # Check if artist page search turned up empty
+    #         if song.find_artist_page() != False:
+                
+    #             # Search for an image
+    #             song.get_artist_image()
+
+
+#     {% extends 'base.html' %}
+
+# {% block title %} TEST {% endblock %}
+
+
+# {% block content %} 
+# <div class="container-fluid col-lg-6">
+# 	<div class="row bg-info">
+# 		<div class="col-md-9"> THIS WEEK {{chart.date}}
+# 		</div>
+# 		<div class="col-md-1"> LAST WEEK
+# 		</div>
+# 		<div class="col-md-1"> PEAK POSITION
+# 		</div>
+# 		<div class="col-md-1"> WEEKS ON CHART
+# 		</div>
+# 	</div>
+        
+#     {% for entry in chart %}
+    
+#         <div class="row">
+#             <div class="col-md-1 text-center align-middle"> {{entry.rank}}
+#             </div>
+#             <div class="col-md-1 text-center align-middle">
+#                 <img class="img-fluid" src="../static/media/missing_album_art.svg" alt="default album image">
+#             </div>
+#             <div class="col-md-7 text-left align-middle">
+#                 {{entry.title | truncate(25) }} by {{entry.artist | truncate(30) }}
+#             </div>
+#             <div class="col-md-1 text-center"> {{entry.lastPos}}
+#             </div>
+#             <div class="col-md-1 text-center">{{entry.peakPos}}
+#             </div>
+#             <div class="col-md-1 text-center">{{entry.weeks}}
+#             </div>
+#         </div>
+        
+#     {% endfor %}
+# </div>
+
+# {% endblock %}
+
+# <!-- <li>
+                    
+#                     {{entry.title}} by {{entry.artist}}
+                    
+#                 </li> -->
+
+#                 <!-- <div class="card" style="width: 18rem">
+#                     <img src="{{ entry.image }}" class="card-img-top" alt=""/>
+#                     <div class="div card-body">
+#                         <img class="card-image" src="../static/media/musical_placeholder.jpg">
+#                         <h5 class="card-title">{{entry.rank}}:{{entry.title}} by {{entry.artist|truncate(20)}}</h5>
+#                         <p class="card-text">Highest position: {{entry.peakPos}} </p>
+#                         <p class="card-text">{{entry.weeks}} weeks on chart</p>
+                        
+#                     </div>
+#                 </div> -->
+
